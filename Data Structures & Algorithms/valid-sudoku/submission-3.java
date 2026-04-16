163f0b4b90c5b84d42c5a3bcf0e9dc5fe248c9cf
JackPool1 <134024062+JackPool1@users.noreply.github.com>
2026-04-15 19:36:21 -0500

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                char val = board[i][j];
+                if(val != '.'){
+                    if(!seen.add(val + "rows" + i) || !seen.add(val + "columns" + j) 
+                    || !seen.add(val + "3X3" + i/3 + '-' + j/3)){
+                        return false;
+                    }
+                    }
+                } 
+            }
+             return true;
+        }
+    }
+
