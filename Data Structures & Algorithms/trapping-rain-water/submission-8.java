395088632467c4927aa243c965bacbfd93352324
JackPool1 <134024062+JackPool1@users.noreply.github.com>
2026-04-27 13:14:02 -0500

    Add: trapping-rain-water - submission-8

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-8.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int res = 0;
+
+        int maxLeft = height[l];
+        int maxRight = height[r];
+
+        while(l < r){
+            if(maxLeft <= maxRight){
+                l++;
+                 maxLeft = Math.max(height[l], maxLeft);
+                res += maxLeft - height[l];
+            }else{
+                r--;
+                maxRight = Math.max(height[r], maxRight);
+                res += maxRight - height[r];
+                }
+            }
+
+            return res;
+        }
+    }
+
